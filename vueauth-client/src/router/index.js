d6556115c799bb1b07cba4b64594384ec9dbf12d
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-07 12:48:29 -0400

    + vue-router auth verification

--- vueauth-client/src/router/index.js
+++ vueauth-client/src/router/index.js
@@ -8,7 +8,7 @@ import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   mode: 'history',
   routes: [
     {
@@ -51,3 +51,32 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) { // If the route has requiresAuth
+    if (localStorage.getItem('jwt') == null) { // Authentication jwt check "Is Logged In"
+      next({ path: '/login', params: { nextUrl: to.fullPath } }) // Redirect to '/login'
+    } else { // Authentication "is valid"
+      let user = JSON.parse(localStorage.getItem('user'))
+      if (to.matched.some(record => record.meta.is_admin)) { // If the route has is_admin
+        if (user.is_admin === 1) { // Check if user has admin rights
+          next()
+        } else {
+          next({ name: 'UserBoard' }) // Redirect to user home if no admin rights
+        }
+      } else {
+        next()
+      }
+    }
+  } else if (to.matched.some(record => record.meta.guest)) { // If the route has guest
+    if (localStorage.getItem('jwt') == null) { // Authentication jwt check "Is Logged In"
+      next()
+    } else {
+      next({ name: 'UserBoard' }) // Redirect to user home if not guest
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
