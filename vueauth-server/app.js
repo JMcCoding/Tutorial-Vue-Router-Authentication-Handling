2a613d4fd0bd00de3dfc4943cf541ceec5dfc536
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-07 11:12:24 -0400

    + server routing functions + CORS function (not complex enough for package) + middleware config + config file

--- vueauth-server/app.js
+++ vueauth-server/app.js
@@ -12,4 +12,95 @@ const database = require('./db')
 const db = new database('sqlitedb')
 const app = express()
 const router = express.Router()
+const port = process.env.PORT || config.port
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+}
+
+// Configuration
+app.use(allowCrossDomain)
+
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+// Creating Routes
+router.post('/register', (req, res) => {
+  db.insert([
+    req.body.name,
+    req.body.email,
+    bcrypt.hashSync(req.body.password, 8)
+  ],
+  err => {
+    if(err) return res.status(500).send('There was a problem registering the user')
+    db.selectByEmail(req.body.email, (err, user) => {
+      if(err) return res.status(500).send('There was a problem getting the user')
+        let token = jwt.sign(
+          { id: user.id },
+          config.secret,
+          { expiresIn: 24 * 60 * 60  * 1000 /* 24 Hours */ })
+
+        res.status(200).send({
+          auth: true,
+          token: token,
+          user: user
+        })
+    })
+  })
+})
+
+router.post('/register-admin', (req, res) => {
+  db.insertAdmin([
+    req.body.name,
+    req.body.email,
+    bcrypt.hashSync(req.body.password, 8),
+    1
+  ],
+  err => {
+    if(err) return res.status(500).send('There was a problem registering the user')
+    db.selectByEmail(req.body.email, (err, user) => {
+      if(err) return res.status(500).send('There was a problem getting the user')
+      let token = jwt.sign(
+        { id: user.id },
+        config.secret,
+        { expiresIn: 24 * 60 * 60  * 1000 /* 24 Hours */ })
+
+      res.status(200).send({
+        auth: true,
+        token: token,
+        user: user
+      })
+    })
+  })
+})
+
+router.post('/login', (req, res) => {
+  db.selectByEmail(req.body.email, (err, user) => {
+    if(err) return res.status(500).send('Error on the server.')
+    if(!user) return res.status(404).send('No user found.')
+
+    let passwordIsValid = bcrypt.compareSync(req.body.password, user.user_pass)
+    if(!passwordIsValid) return res.status(401).send({ auth: false, token: null })
+
+    let token = jwt.sign(
+      { id: user.id },
+      config.secret,
+      { expiresIn: 24 * 60 * 60  * 1000 /* 24 Hours */ })
+    
+    res.status(200).send({
+      auth: true,
+      token: token,
+      user: user
+    })
+  })
+})
+
+
+app.use(router)
+
+let server = app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`)
+})
---
+++ vueauth-server/db/index.js
@@ -0,0 +1,7 @@
+
+
+export default {
+  insert,
+  insertAdmin,
+  selectByEmail
+}
