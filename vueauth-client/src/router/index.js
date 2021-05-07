b736427005de34be80d14a46f56a5f5c14df1633
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-07 12:28:48 -0400

    + client router changes

--- vueauth-client/src/router/index.js
+++ vueauth-client/src/router/index.js
@@ -1,15 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import UserBoard from '@/components/UserBoard'
+import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/dashboard',
+      name: 'UserBoard',
+      component: UserBoard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin,
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
     }
   ]
 })
