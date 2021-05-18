32e946e1a7e56dc271128e3a0db749a991544d6a
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-18 12:22:10 -0400

    Added Register.vue code

--- vueauth-client/src/components/Login.vue
+++ vueauth-client/src/components/Login.vue
@@ -2,8 +2,8 @@
   <div>
     <h4>Login</h4>
     <form>
-      <label for="email">E-Mail Address</label>
       <div>
+        <label for="email">E-Mail Address</label>
         <input id="email" type="email" v-model="email" required autofocus />
       </div>
       <div>
@@ -11,7 +11,7 @@
         <input id="password" type="password" v-model="password" required />
       </div>
       <div>
-        <button type="submit" v-on:click="handleSubmit" value="Login" />
+        <button type="submit" v-on:click="handleSubmit">Login</button>
       </div>
     </form>
   </div>
@@ -26,7 +26,7 @@ export default {
     }
   },
   methods: {
-    handleSubmit: e => {
+    handleSubmit (e) {
       e.preventDefault()
       if (this.password.length > 0) {
         this.$http.post('http://localhost:3000/login', {
---
+++ vueauth-client/src/components/Register.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h4>Register</h4>
+    <div>
+      <label for="name">Name</label>
+      <input id="name" type="text" v-model="name" required/>
+    </div>
+    <div>
+      <label for="email">E-Mail Address</label>
+      <input id="email" type="email" v-model="email" required/>
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input id="password" type="password" v-model="password" required/>
+    </div>
+    <div>
+      <label for="password-confirm">Confirm Password</label>
+      <input id="password-confirm" type="password" v-model="passwordConfirmation" required/>
+    </div>
+    <div>
+      <label for="is-admin">Is this an administrator account?</label>
+      <select id="is-admin" v-model="is_admin">
+        <option value="1">Yes</option>
+        <option value="0">No</option>
+      </select>
+    </div>
+    <div>
+      <button type="submit" v-on:click="handleSubmit">Register</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      is_admin: 0
+    }
+  },
+  methods: {
+    handleSubmit (e) {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
